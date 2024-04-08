9835567d23d097253a9b80cf5282aebecfdbfea0
Alexander Todorov <e-alexander.todorov@smart.com>
2024-04-08 12:43:53 +0300

    WIP Filtering and grouping data

--- src/bin/http_app.rs
+++ src/bin/http_app.rs
@@ -13,8 +13,15 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use data_statistics::services::PriceStatistic::FilterPayload;
+
+use data_statistics::{
+    services::PriceStatistic::{apply_filter, to_generic_json, FilterPayload},
+    PRICE_DATA,
+};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
+use tower_http::cors::CorsLayer;
+
 use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
 use tracing::Span;
 
@@ -32,6 +39,9 @@ async fn main() {
         // `POST /users` goes to `create_user`
         .route("/users", post(create_user))
         .route("/filter", post(filter))
+        .route("/data", post(data))
+        .route("/json", post(json))
+        .layer(CorsLayer::permissive())
         // `TraceLayer` is provided by tower-http so you have to add that as a dependency.
         // It provides good defaults but is also very customizable.
         //
@@ -79,6 +89,7 @@ async fn root() -> &'static str {
 }
 
 async fn filter(Json(payload): Json<FilterPayload>) -> impl IntoResponse {
+    tracing::info!("Filter: {:?}", payload);
     // insert your application logic here
     if let Some(aggregate) = payload.aggregate {
         tracing::info!("Aggregate: {:?}", aggregate);
@@ -88,7 +99,33 @@ async fn filter(Json(payload): Json<FilterPayload>) -> impl IntoResponse {
         tracing::info!("Sort: {:?}", payload.sort);
     }
 
+    if let Some(str_filter) = &payload.filter_string {
+        tracing::info!("Filter string: {:?}", str_filter);
+    }
+
+    if let Some(i32_filter) = &payload.filter_i32 {
+        tracing::info!("Filter i32: {:?}", i32_filter);
+    }
+
+    if let Some(f64_filter) = &payload.filter_f64 {
+        tracing::info!("Filter f64: {:?}", f64_filter);
+    }
+
     (StatusCode::CREATED, Json({}))
+}
+
+async fn data(Json(payload): Json<FilterPayload>) -> impl IntoResponse {
+    let df = apply_filter(&PRICE_DATA, payload);
+    let json = json!({"data": df.to_string()});
+
+    (StatusCode::CREATED, Json(json))
+}
+
+async fn json(Json(payload): Json<FilterPayload>) -> impl IntoResponse {
+    let df = apply_filter(&PRICE_DATA, payload);
+    let json = to_generic_json(&df);
+
+    (StatusCode::CREATED, Json(json))
 }
 
 async fn create_user(
--- src/services/PriceStatistic.rs
+++ src/services/PriceStatistic.rs
@@ -1,10 +1,18 @@
-use std::{collections::HashMap, fmt::Debug};
+use std::{collections::HashMap, f32::consts::E, fmt::Debug, vec};
 
 use polars::{
-    lazy::dsl::{col, lit, Expr},
+    datatypes::DataType,
+    frame::DataFrame,
+    lazy::{
+        dsl::{col, lit, Expr},
+        frame::{IntoLazy, LazyFrame},
+    },
     prelude::Literal,
 };
+
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum PredicateFilter<T: ToOwned + ToString + Debug + Clone + Literal> {
     Like(HashMap<String, T>, bool, bool),
@@ -15,8 +23,31 @@ pub enum PredicateFilter<T: ToOwned + ToString + Debug + Clone + Literal> {
     Gte(HashMap<String, T>, bool),
     Lte(HashMap<String, T>, bool),
 }
+
+enum Compare {
+    Eq,
+    Gt,
+    Lt,
+    Gte,
+    Lte,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Agreggator {
+    source: String,
+    year: String,
+    price: i64,
+}
+
+#[derive(Serialize, Deserialize, Default, Debug, Clone)]
+struct FinalJson<X, Y> {
+    name: String,
+    axis: Vec<X>,
+    data: HashMap<String, Vec<Y>>,
+}
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct FilterPayload {
+    pub group_by: Vec<String>,
     pub aggregate: Option<HashMap<String, Vec<GroupFunc>>>,
     pub filter_string: Option<PredicateFilter<String>>,
     pub filter_i32: Option<PredicateFilter<i32>>,
@@ -27,9 +58,9 @@ pub struct FilterPayload {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum SortBy {
     #[serde(rename = "asc")]
-    Ascending(String, bool, bool),
+    Ascending(String, bool),
     #[serde(rename = "desc")]
-    Descending(String, bool, bool),
+    Descending(String, bool),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -161,13 +192,27 @@ pub fn to_predicate<T: ToOwned + ToString + Debug + Clone + Literal>(
             to_like_predicate(filter, strict, join_and)
         }
         PredicateFilter::In(column, values) => to_in_predicate(&column, values),
-        PredicateFilter::Eq(key_values, join_and) => to_eq_predicate(key_values, join_and),
-        _ => None,
+        PredicateFilter::Eq(key_values, join_and) => {
+            to_compare_predicate(key_values, Compare::Eq, join_and)
+        }
+        PredicateFilter::Gt(key_values, join_and) => {
+            to_compare_predicate(key_values, Compare::Gt, join_and)
+        }
+        PredicateFilter::Lt(key_values, join_and) => {
+            to_compare_predicate(key_values, Compare::Lt, join_and)
+        }
+        PredicateFilter::Gte(key_values, join_and) => {
+            to_compare_predicate(key_values, Compare::Gte, join_and)
+        }
+        PredicateFilter::Lte(key_values, join_and) => {
+            to_compare_predicate(key_values, Compare::Lte, join_and)
+        }
     }
 }
 
-fn to_eq_predicate<T: Debug + ToOwned + ToString + Clone + Literal>(
+fn to_compare_predicate<T: Debug + ToOwned + ToString + Clone + Literal>(
     key_values: HashMap<String, T>,
+    compare: Compare,
     join_and: bool,
 ) -> Option<polars::lazy::dsl::Expr> {
     if key_values.is_empty() {
@@ -176,8 +221,14 @@ fn to_eq_predicate<T: Debug + ToOwned + ToString + Clone + Literal>(
     let mut predicates = vec![];
     for (c, v) in key_values.iter() {
         println!("column {}: value {}", c, v.to_string());
-        let p = col(c).eq(lit(v.clone()));
-        predicates.push(p);
+        let predicate = match compare {
+            Compare::Eq => col(c).eq(lit(v.clone())),
+            Compare::Gt => col(c).gt(lit(v.clone())),
+            Compare::Lt => col(c).lt(lit(v.clone())),
+            Compare::Gte => col(c).gt_eq(lit(v.clone())),
+            Compare::Lte => col(c).lt_eq(lit(v.clone())),
+        };
+        predicates.push(predicate);
     }
     println!("predicates {:?}", predicates);
     let mut predicate = predicates[0].clone();
@@ -196,18 +247,18 @@ pub fn group_by(aggregator: HashMap<String, Vec<GroupFunc>>) -> impl AsRef<[Expr
     for (c, funcs) in aggregator.iter() {
         for f in funcs.iter() {
             let agg_expr = match f {
-                GroupFunc::Min => col(c).min().alias(format!("{}_min", c).as_str()),
-                GroupFunc::Max => col(c).max().alias(format!("{}_max", c).as_str()),
-                GroupFunc::Sum => col(c).sum().alias(format!("{}_sum", c).as_str()),
-                GroupFunc::Median => col(c).median().alias(format!("{}_median", c).as_str()),
-                GroupFunc::Mean => col(c).mean().alias(format!("{}_mean", c).as_str()),
-                GroupFunc::Count => col(c).count().alias(format!("{}_count", c).as_str()),
+                GroupFunc::Min => col(c).min().alias(&format!("{}_min", c)),
+                GroupFunc::Max => col(c).max().alias(&format!("{}_max", c)),
+                GroupFunc::Sum => col(c).sum().alias(&format!("{}_sum", c)),
+                GroupFunc::Median => col(c).median().alias(&format!("{}_median", c)),
+                GroupFunc::Mean => col(c).mean().alias(&format!("{}_mean", c)),
+                GroupFunc::Count => col(c).count().alias(&format!("{}_count", c)),
                 GroupFunc::Quantile(p) => col(c)
                     .quantile(
                         (*p).into(),
                         polars::prelude::QuantileInterpolOptions::Nearest,
                     )
-                    .alias(format!("{}_quantile_{}", c, p).as_str()),
+                    .alias(&format!("{}_quantile_{}", c, p)),
             };
             agg_exprs.push(agg_expr);
         }
@@ -215,12 +266,232 @@ pub fn group_by(aggregator: HashMap<String, Vec<GroupFunc>>) -> impl AsRef<[Expr
     agg_exprs
 }
 
+pub fn sort(df: polars::prelude::LazyFrame, sort: Vec<SortBy>) -> polars::prelude::LazyFrame {
+    let mut sorted = HashMap::new();
+    for s in sort.into_iter() {
+        match s {
+            SortBy::Ascending(column, nulls_last) => {
+                sorted.insert(
+                    column,
+                    polars::prelude::SortOptions {
+                        descending: false,
+                        nulls_last,
+                        ..Default::default()
+                    },
+                );
+            }
+            SortBy::Descending(column, nulls_last) => {
+                sorted.insert(
+                    column,
+                    polars::prelude::SortOptions {
+                        descending: true,
+                        nulls_last,
+                        ..Default::default()
+                    },
+                );
+            }
+        }
+    }
+    let mut df = df;
+    for (by_column, options) in sorted.into_iter() {
+        df = df.sort(&by_column, options);
+    }
+    df
+}
+
+pub fn apply_filter(
+    df: &polars::prelude::DataFrame,
+    filter: FilterPayload,
+) -> polars::prelude::DataFrame {
+    let mut results: LazyFrame = df.clone().lazy();
+    let mut predicates = vec![];
+    if let Some(filter) = filter.filter_string {
+        if let Some(predicate) = to_predicate(filter) {
+            predicates.push(predicate);
+        }
+    }
+    if let Some(filter) = filter.filter_i32 {
+        if let Some(predicate) = to_predicate(filter) {
+            predicates.push(predicate);
+        }
+    }
+    if let Some(filter) = filter.filter_f64 {
+        if let Some(predicate) = to_predicate(filter) {
+            predicates.push(predicate);
+        }
+    }
+    let mut predicate = predicates[0].clone();
+    for p in predicates.iter().skip(1) {
+        predicate = predicate.and(p.clone());
+    }
+    results = results.filter(predicate);
+
+    if let Some(agg) = filter.aggregate {
+        let agg_exprs = group_by(agg.clone());
+        if !filter.group_by.is_empty() {
+            let by = filter.group_by.iter().map(|k| col(k)).collect::<Vec<_>>();
+            results = results.group_by(by).agg(agg_exprs);
+        } else {
+            tracing::error!("Group by columns not provided");
+        }
+    }
+    if !filter.sort.is_empty() {
+        results = sort(results, filter.sort);
+    }
+    results.collect().unwrap()
+}
+fn to_json<X, Y>(axis: Vec<&str>, dataFrame: &DataFrame) -> HashMap<String, FinalJson<X, Y>> {
+    // let y = dataFrame.get_columns();
+
+    // let mut values_i32 = HashMap::new();
+    // let mut values_f64 = HashMap::new();
+    // let mut json = HashMap::new();
+    // y.to_vec().iter().map(f64::to_string).collect::<Vec<_>>();
+    // for i in 0..dataFrame.height() {
+    //     for k in 0..axis.len() {
+    //         if y[k].name() == axis[k] {
+    //             println!("{:?}", y[k].get(i));
+    //         }
+    //     }
+    //     for j in axis.len()..dataFrame.width() {
+    //         println!("{:?}", dataFrame.get_column(j).unwrap().get(i));
+    //     }
+    // }
+    // for i in group_by.len()..y.len() {
+    //     let dtype = y[i].dtype();
+    //     let mut final_json: FinalJson<String, i64> = FinalJson {
+    //         name: y[i].name().to_owned(),
+    //         axis: Vec::new(),
+    //         data: HashMap::new(),
+    //     };
+
+    //     if dtype == &DataType::Int32
+    //         || dtype == &DataType::Int64
+    //         || dtype == &DataType::UInt32
+    //         || dtype == &DataType::UInt64
+    //     {
+    //         let v = y[i].cast(&DataType::Int64).unwrap();
+    //         let slice: Vec<Option<i64>> = v.i64().unwrap().to_vec();
+    //         let mut data = vec![];
+    //         for i in 0..dataFrame.height() {
+    //             let (idx, src, year) = axis[i].clone();
+    //             data.push((idx, src, year, slice[i].unwrap()));
+    //         }
+    //         values_i32.insert(v.name().to_owned(), data.clone());
+    //     } else if dtype == &DataType::Float32 || dtype == &DataType::Float64 {
+    //         let v = y[i].cast(&DataType::Float64).unwrap();
+    //         let slice: Vec<Option<f64>> = v.f64().unwrap().to_vec();
+    //         let mut data = vec![];
+    //         for i in 0..dataFrame.height() {
+    //             let (idx, src, year) = axis[i].clone();
+    //             data.push((idx, src, year, slice[i].unwrap()));
+    //         }
+    //         values_f64.insert(v.name().to_owned(), data.clone());
+    //     } else {
+    //         // let v = y[i].cast(&DataType::String).unwrap();
+    //         // let slice: Vec<Option<String>> = v
+    //         //     .str()
+    //         //     .unwrap()
+    //         //     .into_iter()
+    //         //     .map(|s| match s {
+    //         //         Some(s) => Some(s.to_string()),
+    //         //         None => None,
+    //         //     })
+    //         //     .collect();
+    //         // data_str.insert(v.name().to_owned(), slice.clone());
+    //         // slice
+    //     }
+    // }
+
+    // for (k, values) in values_i32.into_iter() {
+    //     println!("{:?}", k);
+
+    //     let mut final_json: FinalJson<String, i64> = FinalJson {
+    //         name: k.clone(),
+    //         axis: Vec::new(),
+    //         data: HashMap::new(),
+    //     };
+
+    //     for (idx, src, year, value) in values {
+    //         final_json.axis.push(year.clone());
+    //         final_json
+    //             .data
+    //             .entry(src.clone())
+    //             .or_insert_with(Vec::new)
+    //             .push(value);
+
+    //         println!("{} {} {} {} {}", k, idx, src, year, value);
+    //     }
+    //     final_json.axis.sort();
+    //     final_json.axis.dedup();
+    //     json.insert(k, final_json);
+    //     // Serialize to JSON
+    //     let json = serde_json::to_string(&final_json).unwrap();
+    //     println!("{}", json);
+    //     println!("----------------------------------------------------------");
+    // }
+
+    let json = json!(dataFrame.to_string());
+    println!("{}", json);
+    HashMap::new()
+}
+
+pub fn to_generic_json(data: &DataFrame) -> HashMap<String, Value> {
+    let mut json = HashMap::new();
+    let column_values = data.get_columns();
+    json.insert("count".to_owned(), data.height().into());
+    let mut metadata = HashMap::new();
+    let mut idx = 0;
+    let mut meta = vec![];
+    for cv in column_values {
+        metadata.insert("column_name".to_owned(), json!(cv.name()));
+        metadata.insert("column_index".to_owned(), json!(idx));
+        metadata.insert("column_dtype".to_owned(), json!(cv.dtype().to_string()));
+        meta.push(metadata.clone());
+
+        let values = if cv.dtype() == &DataType::Int32 {
+            let values = cv.i32().unwrap().to_vec();
+            json!(values)
+        } else if cv.dtype() == &DataType::Int64 {
+            let values = cv.i64().unwrap().to_vec();
+            json!(values)
+        } else if cv.dtype() == &DataType::UInt32 {
+            let values = cv.u32().unwrap().to_vec();
+            json!(values)
+        } else if cv.dtype() == &DataType::UInt64 {
+            let values = cv.u64().unwrap().to_vec();
+            json!(values)
+        } else if cv.dtype() == &DataType::Float32 {
+            let values = cv.f32().unwrap().to_vec();
+            json!(values)
+        } else if cv.dtype() == &DataType::Float64 {
+            let values = cv.f64().unwrap().to_vec();
+            json!(values)
+        } else {
+            let values = cv
+                .iter()
+                .map(|v| json!(v.get_str().unwrap_or_default()))
+                .collect::<Vec<_>>();
+            json!(values)
+        };
+        json.insert(cv.name().to_owned(), values);
+        idx += 1;
+    }
+    json.insert("metadata".to_owned(), json!(meta));
+    json
+}
+
 #[cfg(test)]
 mod tests {
-    use std::{fs, path::Path};
+    use std::{any::Any, fs, i64, path::Path, vec};
 
     use plotpy::{generate3d, Contour, Curve, Histogram, Plot, RayEndpoint, Surface};
-    use polars::lazy::frame::IntoLazy;
+    use polars::{
+        datatypes::DataType,
+        df,
+        lazy::{dsl::len, frame::IntoLazy},
+    };
+    use serde_json::map;
 
     use crate::PRICE_DATA;
 
@@ -463,5 +734,128 @@ mod tests {
 
         plot.save("resources/curve.jpg").unwrap();
         fs::remove_file("resources/curve.py").unwrap_or_default();
+    }
+
+    #[test]
+    fn test_from_json() {
+        let json = r#"{
+            "group_by": ["source", "year"],
+            "aggregate": {
+                "price": ["max", "count", {"quantile":0.25}]
+            },
+            "sort": [{"asc": ["source", true]}, {"asc": ["year", true]}],
+            "filter_string": {
+                "Like":[{"make":"Mercedes-Benz"}, true, true]
+            },
+            "filter_i32": {
+                "Gte":[{"year":2014}, true]
+            }
+        }"#;
+        let payload: FilterPayload = serde_json::from_str(json).unwrap();
+        let df = apply_filter(&PRICE_DATA, payload);
+        let row_count = df.height();
+        println!("Row count: {}", row_count);
+
+        let x = df.get_column_names();
+        let y = df.get_columns();
+
+        let mut axis = vec![];
+
+        for i in 0..row_count {
+            let first = y[0].get(i).unwrap().to_string();
+            let second = y[1].get(i).unwrap().to_string();
+            axis.push((i, first, second));
+        }
+        let mut values_i32 = HashMap::new();
+        let mut values_f64 = HashMap::new();
+
+        for i in 2..y.len() {
+            let dtype = y[i].dtype();
+
+            if dtype == &DataType::Int32
+                || dtype == &DataType::Int64
+                || dtype == &DataType::UInt32
+                || dtype == &DataType::UInt64
+            {
+                let v = y[i].cast(&DataType::Int64).unwrap();
+                let slice: Vec<Option<i64>> = v.i64().unwrap().to_vec();
+                let mut data = vec![];
+                for i in 0..row_count {
+                    let (idx, src, year) = axis[i].clone();
+                    data.push((idx, src, year, slice[i].unwrap()));
+                }
+                values_i32.insert(v.name().to_owned(), data.clone());
+            } else if dtype == &DataType::Float32 || dtype == &DataType::Float64 {
+                let v = y[i].cast(&DataType::Float64).unwrap();
+                let slice: Vec<Option<f64>> = v.f64().unwrap().to_vec();
+                let mut data = vec![];
+                for i in 0..row_count {
+                    let (idx, src, year) = axis[i].clone();
+                    data.push((idx, src, year, slice[i].unwrap()));
+                }
+                values_f64.insert(v.name().to_owned(), data.clone());
+            } else {
+                // let v = y[i].cast(&DataType::String).unwrap();
+                // let slice: Vec<Option<String>> = v
+                //     .str()
+                //     .unwrap()
+                //     .into_iter()
+                //     .map(|s| match s {
+                //         Some(s) => Some(s.to_string()),
+                //         None => None,
+                //     })
+                //     .collect();
+                // data_str.insert(v.name().to_owned(), slice.clone());
+                // slice
+                continue;
+            }
+        }
+        for (k, values) in values_i32.into_iter() {
+            println!("{:?}", k);
+
+            let mut final_json: FinalJson<String, i64> = FinalJson {
+                name: k.clone(),
+                axis: Vec::new(),
+                data: HashMap::new(),
+            };
+
+            for (idx, src, year, value) in values {
+                final_json.axis.push(year.clone());
+                final_json
+                    .data
+                    .entry(src.clone())
+                    .or_insert_with(Vec::new)
+                    .push(value);
+
+                println!("{} {} {} {} {}", k, idx, src, year, value);
+            }
+            final_json.axis.sort();
+            final_json.axis.dedup();
+
+            // Serialize to JSON
+            let json = serde_json::to_string(&final_json).unwrap();
+            println!("{}", json);
+            println!("----------------------------------------------------------");
+        }
+    }
+
+    #[test]
+    fn test_to_in_predicate() {
+        let json = r#"{
+            "group_by": ["source", "year"],
+            "aggregate": {
+                "price": ["max", "count", {"quantile":0.25}]
+            },
+            "sort": [{"asc": ["source", true]}, {"asc": ["year", true]}],
+            "filter_string": {
+                "Like":[{"make":"Mercedes-Benz"}, true, true]
+            },
+            "filter_i32": {
+                "Eq":[{"year":2014}, true]
+            }
+        }"#;
+        let payload: FilterPayload = serde_json::from_str(json).unwrap();
+        let df = apply_filter(&PRICE_DATA, payload);
+        to_json::<String, i64>(vec!["x"], &df.clone());
     }
 }
